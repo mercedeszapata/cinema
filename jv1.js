50fc371216168988a6b6b55a6cec2f728f85e884
mercedes.zapata <mercedeszapatafigueroa@gmail.com>
2022-05-09 19:23:23 -0300

    DOM y eventos

--- jv1.js
+++ jv1.js
@@ -1,112 +1,58 @@
 alert("Bienvenido a Cinema");
 
-let comprar = prompt("¿Desea comprar entradas?");
-//condiciones para comprar entradas//
 
-if (comprar == "si") {
-    //creación de funcion constructora//
-    function Peliculas(nombrePelicula, edadPermitida, genero) {
+let cambiarModo= document.getElementById("cambiarModo");
+console.log(cambiarModo);
+let fondoBody= document.getElementById("body");
+console.log(fondoBody.className);
+    cambiarModo.addEventListener("click", ()=>{
+        if(fondoBody.className=="claro"){
+            fondoBody.className= "oscuro";
+            divCarterlera.className="oscuro"
+        }
+        else{
+            fondoBody.className="claro";
+            divCarterlera.className="claro";
+        }
+    })
 
+class Peliculas {
+    constructor(nombrePelicula, edadPermitida, genero, id) {
         this.nombreDePelicula = nombrePelicula;
-
         this.edad = edadPermitida;
-
         this.generoDePelicula = genero;
+        this.id = id;
+    }
+}
+const pelicula1 = new Peliculas("Cásate conmigo", "ATP", "Romántica");
+const pelicula2 = new Peliculas("Duna", "ATP", "Fantástica");
+const pelicula3 = new Peliculas("Encanto", "ATP", "Infantil");
+const pelicula4 = new Peliculas("El exorcismo de Dios", "+13", "Terror");
+const pelicula5 = new Peliculas("Scream", "+13", "Terror");
+const pelicula6 = new Peliculas("Ecos de un crimen", "+13", "Suspenso");
+
+let cartelera = [pelicula1, pelicula2, pelicula3, pelicula4, pelicula5];
+let divCarterlera = document.getElementById("divCartelera");
+cartelera.forEach(peliculaEnCartelera => {
+        divCarterlera.innerHTML += `
+         <div class=" card peliculaEnCartelera" id="pelicula${peliculaEnCartelera.id}" style="width: 18rem;">
+  
+  <div class="card-body">
+    <h5 class="card-title">${peliculaEnCartelera.nombreDePelicula}</h5>
+    <p class="card-text">${peliculaEnCartelera.edad}</p>
+    <p class="card-text">${peliculaEnCartelera.generoDePelicula}</p>
+    <button class="btn btn-primary comprarEntrada" id="boton${peliculaEnCartelera.id}><a href="#" class="btn btn-primary"></a>Comprar</button>
+  </div>
+</div>
+         `
     }
 
-    const pelicula1 = new Peliculas("Cásate conmigo", "ATP", "Romántica");
+)
 
-    const pelicula2 = new Peliculas("Duna", "ATP", "Fantástica");
 
-    const pelicula3 = new Peliculas("Encanto", "ATP", "Infantil");
 
-    const pelicula4 = new Peliculas("El exorcismo de Dios", "+13", "Terror");
 
-    const pelicula5 = new Peliculas("Scream", "+13", "Terror");
 
-    const pelicula6 = new Peliculas("Ecos de un crimen", "+13", "Suspenso");
+    
 
-    //array de objetos//
-    let cartelera = [pelicula1, pelicula2, pelicula3, pelicula4, pelicula5];
 
-    let menu = parseInt(prompt("Para conocer las películas disponibles escribir 1, para salir 2"));
-
-    //menu de peliculas//
-    if (menu == 1) {
-
-        let listado = cartelera.map(pelicula => pelicula.nombreDePelicula);
-
-        alert(listado);
-
-        //menu para seleccionar la pelicula a ver//
-        alert("Seleccione la película que desea ver: \n");
-
-        let mensaje = "";
-
-        for (let i = 0; i < listado.length; i++) {
-
-            mensaje = mensaje + (i + 1) + "-" + listado[i] + "\n";
-
-        }
-
-        parseInt(prompt(mensaje));
-
-    } else {
-
-        alert("¡Gracias por visitar nuestra página!");
-    }
-
-    let cant = parseInt(prompt("¿Cuántas entradas desea comprar?"));
-
-    let precio;
-
-    precio = calcularPrecio(cant);
-
-    alert("El valor de las entradas es $" + precio + ".");
-
-    pagar();
-
-} else {
-
-    alert("No te pierdas los estrenos de esta semana.");
-}
-
-//funcion para calcular valor a pagar por las entradas//
-function calcularPrecio(_cant) {
-
-    _precio = 250 * _cant;
-
-    return _precio;
-
-}
-
-//funcion para seleccionar los medios de pago//
-function pagar() {
-
-    let medioDePago = prompt("Proceder a pagar. Elegir efectivo o tarjeta");
-
-    while (medioDePago != "no") {
-
-        switch (medioDePago) {
-
-            case "efectivo":
-
-                alert("Tu código de reserva es 1234. Acercate a nuestra sucursal para terminar la compra.");
-
-                break;
-
-            case "tarjeta":
-
-                prompt("Ingresa el número de tu tarjeta, fecha de vencimiento y código de seguridad");
-
-                alert("Gracias por tu compra. ¡Que disfrutes la función!");
-
-                break;
-
-        }
-
-        break;
-
-    }
-
-}
