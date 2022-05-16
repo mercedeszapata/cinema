f8ce99a0edfbafb80f5a46b720f24dfa7b4f368a
mercedes.zapata <mercedeszapatafigueroa@gmail.com>
2022-05-16 20:38:39 -0300

    Segunda entrega

--- jv1.js
+++ jv1.js
@@ -32,14 +32,15 @@ class Peliculas {
         this.id = id;
     }
 }
-const pelicula1 = new Peliculas("Cásate conmigo", "ATP", "Romántica");
-const pelicula2 = new Peliculas("Duna", "ATP", "Fantástica");
-const pelicula3 = new Peliculas("Encanto", "ATP", "Infantil");
-const pelicula4 = new Peliculas("El exorcismo de Dios", "+13", "Terror");
-const pelicula5 = new Peliculas("Scream", "+13", "Terror");
-const pelicula6 = new Peliculas("Ecos de un crimen", "+13", "Suspenso");
+const pelicula1 = new Peliculas("Cásate conmigo", "ATP", "Romántica",1);
+const pelicula2 = new Peliculas("Duna", "ATP", "Fantástica",2);
+const pelicula3 = new Peliculas("Encanto", "ATP", "Infantil",3);
+const pelicula4 = new Peliculas("El exorcismo de Dios", "+13", "Terror",4);
+const pelicula5 = new Peliculas("Scream", "+13", "Terror",5);
+const pelicula6 = new Peliculas("Ecos de un crimen", "+13", "Suspenso",6);
 
 let cartelera = [pelicula1, pelicula2, pelicula3, pelicula4, pelicula5];
+let carrito=[];
 let divCarterlera = document.getElementById("divCartelera");
 cartelera.forEach(peliculaEnCartelera => {
         divCarterlera.innerHTML += `
@@ -49,13 +50,25 @@ cartelera.forEach(peliculaEnCartelera => {
     <h5 class="card-title">${peliculaEnCartelera.nombreDePelicula}</h5>
     <p class="card-text">${peliculaEnCartelera.edad}</p>
     <p class="card-text">${peliculaEnCartelera.generoDePelicula}</p>
-    <button class="btn btn-primary comprarEntrada" id="boton${peliculaEnCartelera.id}><a href="#" class="btn btn-primary"></a>Comprar</button>
+    <button id= "boton${peliculaEnCartelera.id}"class="btn btn-primary comprarEntrada" id="boton${peliculaEnCartelera.id}><a href="#" class="btn btn-primary"></a>Comprar entrada</button>
   </div>
 </div>
          `
-    }
+    
 
+    }
 )
+
+
+cartelera.forEach(peliculaEnCartelera=>{
+    document.getElementById(`boton${peliculaEnCartelera.id}`).addEventListener("click", ()=> {
+        
+    carrito.push(peliculaEnCartelera.nombreDePelicula)
+    localStorage.setItem("productosCarrito", JSON.stringify(carrito))
+    })
+})
+
+    
 
 
 
